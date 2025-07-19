270ef274509fb45d555e9a5541f46d27e688095a
Jack Temple <220726590+fullstack-jack@users.noreply.github.com>
2025-07-19 20:14:40 +0100

    Added hover effects

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
 const container = document.querySelector(".container")
 
-let gridSize = 32
+// GRID
+let gridSize = 16;
 
-for (let i = 0; i <= gridSize*gridSize; i++) {    
-    const cell = document.createElement("div")
-    cell.classList.add('cell')
-    container.append(cell)
+function createGrid(gridSize) {
+    container.innerHTML = ''
+    const cellSize = 400 / gridSize;
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add('cell');
+        cell.style.width = `${cellSize}px`;
+        cell.style.height = `${cellSize}px`;
+        cell.addEventListener("mouseenter", () => {
+            cell.style.backgroundColor = 'black';
+        })
+        container.appendChild(cell);
+    }
 }
 
+//RESIZE
+const button = document.querySelector("button")
+button.addEventListener('click', () => {
+  let newSize = prompt("Enter grid size (1 to 100):", 16);
+
+  newSize = parseInt(newSize);
+
+  if (isNaN(newSize) || newSize < 1 || newSize > 100) {
+    alert("Invalid input. Please enter a number between 1 and 100.");
+    return;
+  }
+
+  gridSize = newSize;
+  createGrid(gridSize);
+});
+
+createGrid(gridSize);
